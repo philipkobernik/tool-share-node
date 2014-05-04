a85614d9c193fb2d31c6408ea97d7abb590887a8
Philip Kobernik <ptk921@gmail.com>
2014-05-03 17:39:54 -0700

    react demo in page.

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 var Hapi = require('hapi');
-var server = Hapi.createServer('localhost', process.env.PORT || 4000);
+
+var server = new Hapi.Server(process.env.PORT || 4000, 'localhost', {
+  views: {
+    engines: {
+      jade: "jade"
+    },
+    path: "./views"
+  }
+});
 
 server.route([{
   method: 'GET',
   path: '/',
   handler: function(request, reply) {
-    reply('tools!');
+
+    reply.view('index', {
+      name: request.query.name
+    });
   }
 }]);
+
+server.route({
+  path: "/build/{path*}",
+  method: "GET",
+  handler: {
+    directory: {
+      path: "./build",
+      listing: false,
+      index: false
+    }
+  }
+});
 
 server.start();
 
